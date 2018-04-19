1e29880f597ab69bb3328eb251da2c91efe84b80
Snowflake <notme@airmail.cc>
2018-04-19 18:51:27 +0000

    Add hashbang#!, deps and roadmap

--- stick_export.sh
+++ stick_export.sh
@@ -1,10 +1,14 @@
+#!/bin/bash
+
 # stick_export.sh by 26000 (https://github.com/26000/tg-sticker-export)
 # License: BSD 3-clause
 
-# Usage: ./stick_export.sh <stickerpack name or URL>
+## Usage: ./stick_export.sh <stickerpack name or URL>
 #
-# will download all stickers in the stickerset to a <stickername> dir, which
+# Will download all stickers in the stickerset to a <stickername> dir, which
 # contains two subdirs: webp and png, for respective file formats.
+#
+# Be sure to insert your bot token (below) before use.
 #
 # In order to get the stickerpack URL, do the following:
 # (using TDesktop)
@@ -13,12 +17,27 @@
 # 3. Click 'Share stickers'
 # 4. Paste the link as an argument to this script
 
+## Dependencies
+# - bash or something compatible
+# - curl, wget (for http requests)
+# - ffmpeg (for convertation to png)
+# - jq (!) (for json parsing)
+# - sed (for parsing parsed json)
+
 ### SETTINGS ###
 # You need to get a bot token. Go talk to https://t.me/BotFather, write
 # '/newbot' and follow its instructions. Eventually you get a token.
 # You can just wait for something scary to appear in the chat and that is a token.
 TOKEN=
 ### END OF SETTINGS ###
+
+## TODO
+# - a nice README
+# - settings to decide wether to convert stickers to png or leave in webp
+# - a token checker which will tell the user if something's wrong with it
+# - cli flags like help or to specify the output directory
+# - make it quiet? and use a flag for verbosity
+# - [your feature here]
 
 # Don't edit anything below this line if you don't know what you are doing!
 
